67532af4753c2b5f98076747a450d4f8f0b73280
James Kingsley <jameslkingsley@outlook.com>
2016-10-07 18:57:37 +0100

    Add blank component

---
+++ extras/blank/README.md
@@ -0,0 +1,7 @@
+arcmf_blank
+===========
+Blank component.
+
+## Maintainers
+The people responsible for merging changes to this component or answering potential questions.
+- [Kingsley](https://github.com/jameslkingsley)
---
+++ extras/blank/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"arcmf_main"};
+        author = ECSTRING(main,ARCMFTeam);
+        authors[] = {"Kingsley"};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgEden.hpp"
